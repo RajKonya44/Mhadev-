d8e9d51602faeaa6b7f7a42c8d91b9f50ed1e462
Raj <143399883+RajKonya44@users.noreply.github.com>
2024-06-08 17:59:31 +0600

    Update 0.py

--- 0.py
+++ 0.py
@@ -2,13 +2,13 @@ from colorama import Fore, Style
 import time
 
 def animate_text(text, repeat=5):
-    colors = [Fore.WHITE, Fore.RED, Fore.BLUE, Fore.G>
+    colors = [Fore.WHITE, Fore.RED, Fore.BLUE, Fore.GREEN]  # Fixe>
     for _ in range(repeat):
         for i, char in enumerate(text):
-            color = colors[i % len(colors)]  # Cycle >
+            color = colors[i % len(colors)]  
             print(color + char, end='', flush=True)
-            time.sleep(0.1)  # Adjust the sleep time >
-        print(Style.RESET_ALL)  # Reset color to defa>
+            time.sleep(0.1)  
+        print(Style.RESET_ALL)  
 
 text = "Radha Krishna"
 animate_text(text, repeat=100)
