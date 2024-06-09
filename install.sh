b63e0555fe417990ca9c656391917bf4b5363f57
Raj <143399883+RajKonya44@users.noreply.github.com>
2024-06-09 12:42:20 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
 
+clear
+
+spinner() {
+  local i sp n
+  sp='/-\|'
+  n=${#sp}
+  printf ' '
+  while sleep 0.1; do
+    printf "%s\b" "${sp:i++%n:1}"
+  done
+}
+
+spinner &
+
+msg="wellcome everyone"
+
+for ((i=0; i<${#msg}; i++)); do
+    echo -ne "\e[$((31 + i % 6))m${msg:$i:1}"
+done
+
+for ((i=1; i<=10; i++)); do
+    echo ""
+    echo "$msg"
+done
+
+
+sleep 5
+
+kill "$!" 2>/dev/null
+
+echo "echo 'made by me package you are welcome'"
+
 # Update the package list
 echo "Updating package list..."
 pkg update -y
@@ -19,6 +51,8 @@ echo "Installing requests"
 pip install requests
 echo "Installing colorama"
 pip install colorama
+echo "Installing urlscanio"
+pip install urlscanio
 # Install BeautifulSoup
 echo "Installing BeautifulSoup..."
 pip install beautifulsoup4
